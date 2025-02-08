eebe1af113e2bb1734d193f5e24ef0d7e643ae69
Andre Proenca <andreproenca@hotmail.com>
2025-02-08 18:28:16 -0400

    Basic beans validation

--- backend/src/main/java/com/softcraft/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/softcraft/dscatalog/dto/UserDTO.java
@@ -6,12 +6,17 @@ import java.util.Set;
 
 import com.softcraft.dscatalog.entities.User;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	@NotBlank(message = "Campo obrigatório")
 	private String firstName;
 	private String lastName;
+	@Email(message = "Favor entrar um email válido")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<>();
